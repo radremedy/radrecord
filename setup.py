b7bb1f3cbfe4bcfb229509a0f3c0db1f91d504d1
Cate S <cate@funoninter.net>
2014-09-08 23:09:27 -0400

    updating version and normalizing fields

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "radrecord",
     packages = ["radrecord"],
-    version = "0.0.2",
+    version = "0.0.3",
     description = "A data format to be used throughout the Rad Remedy project",
     url = "https://github.com/radremedy/radrecord",
 )
