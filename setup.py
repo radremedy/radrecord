b0cb6c06252a083fc8edd69826b82c952f23aaba
Cate S <cate@funoninter.net>
2014-12-06 00:01:23 -0500

    adding hours/NPI fields to the RadRecord format

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "radrecord",
     packages = ["radrecord"],
-    version = "1.0.1",
+    version = "1.1.0",
     description = "A data format to be used throughout the Rad Remedy project",
     url = "https://github.com/radremedy/radrecord",
 )
